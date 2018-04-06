1f6cd40261adcc1bb47c3683a8996245d1dd475c
Dione <dione.estabillo@gmail.com>
2018-04-06 21:49:21 +0000

    Add more student details

--- directory.rb
+++ directory.rb
@@ -1,16 +1,22 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the student's details"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
+  puts "Student Name: "
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
+     puts "Date of Birth: "
+     birthday = gets.chomp
+     puts "Citizenship: "
+     country = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, birthday: birthday, country: country}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Enter next student's details"
     name = gets.chomp
   end
   # return the array of students
@@ -23,7 +29,8 @@ end
 def print(students)
     accumulator = 0
     while accumulator < students.length
-      puts "#{students[accumulator][:name]} (#{students[accumulator][:cohort]} cohort)"
+      puts "#{students[accumulator][:name]} (#{students[accumulator][:cohort]} cohort),
+      born on #{students[accumulator][:birthday]}, citizen of #{students[accumulator][:country]} "
       accumulator += 1
     end
 end
