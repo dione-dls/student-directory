d383410f05133c71f827e6ea9582e1c481a28244
Dione <dione.estabillo@gmail.com>
2018-04-07 11:25:48 +0000

    Add input feature and default value for cohort

--- directory.rb
+++ directory.rb
@@ -8,12 +8,15 @@ def input_students
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
-     puts "Date of Birth: "
+     puts "Cohort: "
+     cohort = gets.chomp
+     cohort = "unknown" if cohort == ""
+     puts "Date of Birth (e.g. 01 Jan 1985): "
      birthday = gets.chomp
      puts "Citizenship: "
      country = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november, birthday: birthday, country: country}
+    students << {name: name, cohort: cohort.to_sym, birthday: birthday, country: country}
     puts "Now we have #{students.count} students"
     # get another name from the user
     puts "Enter next student's details"
@@ -23,18 +26,18 @@ def input_students
   students
 end
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(75)
+  puts "-------------".center(75)
 end
 def print(students)
-    accumulator = 0
-    while accumulator < students.length
-      puts "#{students[accumulator][:name]} (#{students[accumulator][:cohort]} cohort), born on #{students[accumulator][:birthday]}, citizen of #{students[accumulator][:country]}".center(60)
-      accumulator += 1
-    end
+  students.each { |student, index|
+    puts "#{student[:name]} (#{student[:cohort]} cohort) " +
+ "Date of Birth: #{student[:birthday]} ".center(35) +
+ "Country of Citizenship: #{student[:country]}"
+  }
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+  puts "Overall, we have #{students.count} great students".center(75)
 end
 
 students = input_students
