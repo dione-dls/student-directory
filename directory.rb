7f44a6196da35cdc34f1eea8fe8f45ead85870ad
Dione <dione.estabillo@gmail.com>
2018-04-11 12:51:12 +0000

    Add load data from file functionality

--- directory.rb
+++ directory.rb
@@ -1,5 +1,14 @@
 @students = [] # an empty array accessible to all methods
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, birthday, country = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, birthday: birthday, country: country}
+  end
+  file.close
+end
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -47,6 +56,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -64,6 +74,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
