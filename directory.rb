18ce149da70ff555e164c7c9e08ae94577106a17
Dione <dione.estabillo@gmail.com>
2018-04-11 11:11:44 +0000

    Refactor the interactive menu

--- directory.rb
+++ directory.rb
@@ -1,33 +1,12 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-    end
-  end
-end
+@students = [] # an empty array accessible to all methods
 
 def input_students
   puts "Please enter the student's details"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
+
   # get the first name
   puts "Student Name: "
-  name = gets.tr("\n\r","")
+  name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
      puts "Cohort: "
@@ -38,24 +17,56 @@ def input_students
      puts "Citizenship: "
      country = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, birthday: birthday, country: country}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym, birthday: birthday, country: country}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     puts "Enter next student's details"
     puts "Student Name: "
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy".center(75)
   puts "-------------".center(75)
 end
-def print(students)
-  if !students.empty?
+
+def print_student_list
+  if !@students.empty?
     cohorts = []
-    students.each { |student|
+    @students.each { |student|
       cohorts.push(student[:cohort])
     }
   end
@@ -63,7 +74,7 @@ def print(students)
   if cohorts != nil
     cohorts.uniq.each { |cohort|
       puts "Cohort: #{cohort}"
-    students.select { |student|
+    @students.select { |student|
       puts "#{student[:name]}" +
       "Date of Birth: #{student[:birthday]} ".center(35) +
       "Country of Citizenship: #{student[:country]}" if student[:cohort] == cohort
@@ -71,9 +82,10 @@ def print(students)
     }
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(75) if students.count > 1
-  puts "Overall, we have #{students.count} great student".center(75) if students.count == 1
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(75) if @students.count > 1
+  puts "Overall, we have #{@students.count} great student".center(75) if @students.count == 1
 end
 
-students = interactive_menu
+interactive_menu
